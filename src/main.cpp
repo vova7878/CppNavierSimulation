2e0f5f63950a6e51c78fba21468fa7c348d2f1b3
vova7878 <vova7878@users.noreply.github.com>
2022-11-04 18:55:05 +0700

    add compute shader for test

--- src/main.cpp
+++ src/main.cpp
@@ -7,40 +7,30 @@
 #include "ShaderUtils.hpp"
 #include "TextureUtils.hpp"
 
-template<typename T, size_t size>
-struct FastLinearFilter {
+template<typename T>
+struct Counter {
 private:
-    size_t index;
     size_t elements;
     T sum;
-    T buff[size];
 
 public:
 
-    FastLinearFilter() : index(0), elements(0), sum(0) {
-        for (size_t i = 0; i < size; i++) {
-            buff[i] = 0;
-        }
+    Counter() : elements(0), sum(0) { }
+
+    void push(T v) {
+        elements++;
+        sum += v;
     }
 
-    void push(double v) {
-        if (elements < size) {
-            elements++;
-        }
-        sum += v - buff[index];
-        buff[index] = v;
-        index = (index + 1) % size;
-    }
-
-    void resum() {
+    void clear() {
+        elements = 0;
         sum = 0;
-        for (size_t i = 0; i < size; i++) {
-            sum += buff[i];
-        }
     }
 
     T get() {
-        return sum / elements;
+        T out = sum / elements;
+        clear();
+        return out;
     }
 };
 
@@ -61,11 +51,17 @@ debug_callback(GLenum source,
 
 struct MainRenderer : public gl_utils::Renderer {
     gl_utils::Window* window;
-    int w_width, w_height;
+    int render_width, render_height;
     GLuint vaoID;
     GLuint vboID;
     GLint positionLocation;
+    GLint dataSizeLocation;
     GLint renderSizeLocation;
+
+    GLuint programID1;
+    GLuint programID2;
+
+    constexpr static int data_width = 400, data_height = 400;
 
     virtual void onCreate(gl_utils::Window* w) override {
         window = w;
@@ -73,10 +69,12 @@ struct MainRenderer : public gl_utils::Renderer {
         glEnable(GL_DEBUG_OUTPUT);
         glDebugMessageCallback(debug_callback, 0);
 
+        int comp = gl_utils::createFileShader(GL_COMPUTE_SHADER, "shaders/compute.comp");
+        programID2 = gl_utils::createProgram(comp);
+
         int vert = gl_utils::createFileShader(GL_VERTEX_SHADER, "shaders/simple.vert");
         int frag = gl_utils::createFileShader(GL_FRAGMENT_SHADER, "shaders/screen.frag");
-        int programID = gl_utils::createProgram(vert, frag);
-        glUseProgram(programID);
+        programID1 = gl_utils::createProgram(vert, frag);
 
         glGenVertexArrays(1, &vaoID);
         glBindVertexArray(vaoID);
@@ -91,37 +89,39 @@ struct MainRenderer : public gl_utils::Renderer {
         vboID = gl_utils::genBuffer(GL_ARRAY_BUFFER, sizeof (vertices), vertices, GL_STATIC_DRAW);
         glBindBuffer(GL_ARRAY_BUFFER, vboID);
 
-        positionLocation = glGetAttribLocation(programID, "position");
+        positionLocation = glGetAttribLocation(programID1, "position");
         glVertexAttribPointer(positionLocation, 2, GL_FLOAT, false, 0, 0);
 
         glBindVertexArray(0);
 
-        constexpr int width = 400, height = 400;
-
-        float tmp[width * height];
-        for (int i = 0; i < width * height; i++) {
-            tmp[i] = float(width - i % width) * float(i / width) / width / height;
+        float tmp[data_width * data_height];
+        for (int i = 0; i < data_width * data_height; i++) {
+            tmp[i] = float(data_width - i % data_width) * float(i / data_width) / data_width / data_height;
         }
-
-        GLint dataSizeLocation = glGetUniformLocation(programID, "dataSize");
-        glUniform2i(dataSizeLocation, width, height);
-        renderSizeLocation = glGetUniformLocation(programID, "renderSize");
 
         GLint ssbo = gl_utils::genBuffer(GL_SHADER_STORAGE_BUFFER, sizeof (tmp), tmp, GL_STATIC_DRAW);
 
-        GLint block_index = glGetProgramResourceIndex(programID, GL_SHADER_STORAGE_BLOCK, "ssbo1");
-        glShaderStorageBlockBinding(programID, block_index, 1);
+        GLint block_index = glGetProgramResourceIndex(programID1, GL_SHADER_STORAGE_BLOCK, "ssbo1");
+        glShaderStorageBlockBinding(programID1, block_index, 1);
+
+        block_index = glGetProgramResourceIndex(programID2, GL_SHADER_STORAGE_BLOCK, "ssbo1");
+        glShaderStorageBlockBinding(programID2, block_index, 1);
+
         glBindBufferBase(GL_SHADER_STORAGE_BUFFER, 1, ssbo);
+
+        dataSizeLocation = glGetUniformLocation(programID1, "dataSize");
+        renderSizeLocation = glGetUniformLocation(programID1, "renderSize");
     }
 
-    decltype(std::chrono::high_resolution_clock::now())
-    time1 = std::chrono::high_resolution_clock::now();
-    decltype(std::chrono::high_resolution_clock::now())
-    time2 = std::chrono::high_resolution_clock::now();
-    FastLinearFilter<double, 64> f;
+    using clock_type = std::chrono::high_resolution_clock;
+    using time_type = std::chrono::time_point<clock_type>;
+
+    time_type time1 = clock_type::now();
+    time_type time2 = time1;
+    Counter<double> f;
 
     void showFPS() {
-        auto tmp_time = std::chrono::high_resolution_clock::now();
+        auto tmp_time = clock_type::now();
         f.push(std::chrono::duration_cast<std::chrono::duration<double>>(tmp_time - time1).count());
         time1 = tmp_time;
         if (std::chrono::duration_cast<std::chrono::duration<double>>(tmp_time - time2).count() > 0.1) {
@@ -132,13 +132,21 @@ struct MainRenderer : public gl_utils::Renderer {
     }
 
     virtual void onDraw() override {
+        glUseProgram(programID2);
+
+        glDispatchCompute(400, 400, 1);
+        glMemoryBarrier(GL_SHADER_STORAGE_BARRIER_BIT);
+
+        glUseProgram(programID1);
+
         glClearColor(0.5f, 0.5f, 0.5f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
         glBindVertexArray(vaoID);
         glEnableVertexAttribArray(positionLocation);
 
-        glUniform2i(renderSizeLocation, w_width, w_height);
+        glUniform2i(dataSizeLocation, data_width, data_height);
+        glUniform2i(renderSizeLocation, render_width, render_height);
 
         glDrawArrays(GL_TRIANGLE_FAN, 0, 4);
 
@@ -149,9 +157,9 @@ struct MainRenderer : public gl_utils::Renderer {
     }
 
     virtual void onChangeSize(int width, int height) override {
-        w_width = width;
-        w_height = height;
-        glViewport(0, 0, w_width, w_height);
+        render_width = width;
+        render_height = height;
+        glViewport(0, 0, render_width, render_height);
     }
 
     virtual void onDispose() override { }
