7157c41eea6913b6fe79737b7d457463c3595ca8
vova7878 <46866705+vova7878@users.noreply.github.com>
2022-11-13 16:06:51 +0700

    add linux_run script

---
+++ linux_run.sh
@@ -0,0 +1,12 @@
+if [ ! -d "build" ]; then
+  mkdir build
+fi
+cd build
+cmake ..
+if [ $? -eq 0 ]; then
+    make clean
+    make -j $(nproc --all)
+fi
+if [ $? -eq 0 ]; then
+    ./simulation
+fi 
